2d9e4998c79c89610e773f3471664e8b1cbc78ae
lauraschultz <schultzlaurac@gmail.com>
2021-02-06 19:09:37 +0100

    whoopsies

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	},
 	purge: {
 		enabled: process.env.NODE_ENV === "production",
-		content: ["./src/*.svelte"],
+		content: ["./src/**/*.svelte"],
 	},
 	theme: {
 		fontFamily: {
